5c161fe4985cf85824eab4f028846cf5f12cb00c
Cris415 <cristianreynoso91@gmail.com>
2017-07-22 22:07:36 -0400

    basic timer just to see if it works

---
+++ script.js
@@ -0,0 +1,41 @@
+var myVar = setInterval(myTimer, 1000);
+var secondsElapsed = 0;
+var seconds = 0;
+var minutes = 0;
+var hours = 0;
+var lengthOfTimer = 5; //seconds;
+function timerDone(lengthOfTime) {
+  if (lengthOfTime === secondsElapsed) {
+    clearTimeout(myVar);
+  }
+}
+
+function myTimer() {
+  // var d = new Date();
+  // // document.getElementById("display").innerHTML = d.toLocaleTimeString();
+
+  secondsElapsed++;
+  seconds++;
+  timerDone(lengthOfTimer);
+  if (seconds === 60) {
+    seconds = 0;
+    minutes++;
+  }
+  if (minutes === 60) {
+    minutes = 0;
+    hours++;
+  }
+
+  console.log(twoDigits(hours) + ':' +
+    twoDigits(minutes) + ':' +
+    twoDigits(seconds));
+}
+
+function twoDigits(num) {
+  if (num.toString().length < 2) {
+    num = "0" + num;
+    //num = parseInt(num);
+    return num;
+  }
+  return num;
+}
